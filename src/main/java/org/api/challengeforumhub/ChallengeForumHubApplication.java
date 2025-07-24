1fcba85be109453abd748e3689995c2e42607855
Rob <robertorodriguesre@gmail.com>
2025-07-24 11:24:13 -0300

    commit inicial

---
+++ src/main/java/org/api/challengeforumhub/ChallengeForumHubApplication.java
@@ -0,0 +1,13 @@
+package org.api.challengeforumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumHubApplication.class, args);
+	}
+
+}
